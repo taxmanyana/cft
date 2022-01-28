635695367cfe17da9a3149df5fd98fb3c5062b69
Thembani Moitlhobogi <taxmanyana@gmail.com>
2022-01-28 16:30:13 +0100

    use quotes on Windows paths variables - fixes #13

--- install-qgis-modules.bat
+++ install-qgis-modules.bat
@@ -2,82 +2,96 @@
 echo.
 echo CFT Installation Script
 echo.
+echo ______________________________
 chcp 437
 SET mypath=%~dp0
 set mypath=%mypath:~0,-1%
 
-set OSGEO4W_ROOT=""
 rem Detect QGIS Version
 FOR /F "tokens=* USEBACKQ" %%g in (`dir /b "%PROGRAMFILES(X86)%\QGIS 3*" 2^>NUL`) do (
-  set OSGEO4W_ROOT="%PROGRAMFILES(X86)%\%%g"
+  set QGISDIR=%%g
 )
-FOR /F "tokens=* USEBACKQ" %%g in (`dir /b "%PROGRAMFILES%\QGIS 3*" 2^>NUL`) do (
-  set OSGEO4W_ROOT="%PROGRAMFILES%\%%g"
-)
+if not "%QGISDIR%" == "" set OSGEO4W_ROOT=%PROGRAMFILES(X86)%\%QGISDIR%
 
-if %OSGEO4W_ROOT% == "" (
+FOR /F "tokens=* USEBACKQ" %%g in (`dir /b "%PROGRAMFILES%\QGIS 3*" 2^>NUL`) do (
+  set QGISDIR=%%g
+)
+if not "%QGISDIR%" == "" set OSGEO4W_ROOT=%PROGRAMFILES%\%QGISDIR%
+
+if "%QGISDIR%" == "" (
   echo.
   echo could not find any QGIS v3 installation.
   pause
   exit
 )
 
+rem write the OSGEO4W_ROOT variable to qgis_env.bat
 echo set OSGEO4W_ROOT=%OSGEO4W_ROOT%> "%mypath%\qgis_env.bat"
-set QGISAPP=""
-set QGISAP=""
-FOR /F "tokens=* USEBACKQ" %%g in (`dir /b %OSGEO4W_ROOT%\apps\qgis* 2^>NUL`) do (
-  set QGISAPP=%OSGEO4W_ROOT%\apps\%%g
+
+FOR /F "tokens=* USEBACKQ" %%g in (`dir /b "%OSGEO4W_ROOT%\apps\qgis*" 2^>NUL`) do (
   set QGISAP=%%g
 )
-set QTAPP=""
-FOR /F "tokens=* USEBACKQ" %%g in (`dir /b %OSGEO4W_ROOT%\apps\qt* 2^>NUL`) do (
-  set QTAPP=%OSGEO4W_ROOT%\apps\%%g
+if not "%QGISAP%" == ""  set QGISAPP=%OSGEO4W_ROOT%\apps\%QGISAP%
+
+FOR /F "tokens=* USEBACKQ" %%g in (`dir /b "%OSGEO4W_ROOT%\apps\qt*" 2^>NUL`) do (
+  set QTAP=%%g
 )
+if not "%QTAP%" == "" set QTAPP=%OSGEO4W_ROOT%\apps\%QTAP%
 set PYAPP=""
-FOR /F "tokens=* USEBACKQ" %%g in (`dir /b %OSGEO4W_ROOT%\apps\Python* 2^>NUL`) do (
-  set PYAPP=%OSGEO4W_ROOT%\apps\%%g
+FOR /F "tokens=* USEBACKQ" %%g in (`dir /b "%OSGEO4W_ROOT%\apps\Python*" 2^>NUL`) do (
+  set PYAP=%%g
 )
+if not "%QTAP%" == "" set PYAPP=%OSGEO4W_ROOT%\apps\%PYAP%
 
-if exist %QGISAPP% echo set QGISAPP=%QGISAPP%>> "%mypath%\qgis_env.bat"
-if exist %QGISAPP% echo set QGISAP=%QGISAP%>> "%mypath%\qgis_env.bat"
-if exist %QTAPP% echo set QTAPP=%QTAPP%>> "%mypath%\qgis_env.bat"
-if exist %PYAPP% echo set PYAPP=%PYAPP%>> "%mypath%\qgis_env.bat"
+rem write the environment variables to qgis_env.bat
+if exist "%QGISAPP%" echo set QGISAPP=%QGISAPP%>> "%mypath%\qgis_env.bat"
+if exist "%QGISAPP%" echo set QGISAP=%QGISAP%>> "%mypath%\qgis_env.bat"
+if exist "%QTAPP%" echo set QTAPP=%QTAPP%>> "%mypath%\qgis_env.bat"
+if exist "%PYAPP%" echo set PYAPP=%PYAPP%>> "%mypath%\qgis_env.bat"
 
-if exist %OSGEO4W_ROOT%\bin\o4w_env.bat call %OSGEO4W_ROOT%\bin\o4w_env.bat
-if exist "%mypath%\qgis_env.bat" call "%mypath%\qgis_env.bat"
+rem load the environment
+if exist "%mypath%\qgis_env.bat" goto envexists
+echo qgis_env.bat not found in the CFT folder. run the install-qgis-modules.bat again
+pause
+exit
 
+:envexists
+call "%mypath%\qgis_env.bat"
+if exist "%OSGEO4W_ROOT%"  goto qgisexists
+echo QGIS folder does not exist, check QGIS and run the install-qgis-modules.bat again
+pause
+exit
 
-if not exist %OSGEO4W_ROOT% echo %OSGEO4W_ROOT% does not exist
-if not exist %QGISAPP% echo %QGISAPP% does not exist
-if not exist %PYAPP% echo %PYAPP% does not exist
-if not exist %QTAPP% echo %QTAPP% does not exist
-if not exist %OSGEO4W_ROOT%\bin\o4w_env.bat echo o4w_env.bat does not exist
-if not exist %OSGEO4W_ROOT%\bin\qt5_env.bat echo qt5_env.bat does not exist
-if not exist %OSGEO4W_ROOT%\bin\py3_env.bat echo py3_env.bat does not exist
+:qgisexists
+if not exist "%QGISAPP%" echo QGISAPP folder does not exist
+if not exist "%PYAPP%" echo PYAPP folder does not exist
+if not exist "%QTAPP%" echo QTAPP folder does not exist
+if not exist "%OSGEO4W_ROOT%\bin\o4w_env.bat" echo o4w_env.bat does not exist
+if not exist "%OSGEO4W_ROOT%\bin\qt5_env.bat" echo qt5_env.bat does not exist
+if not exist "%OSGEO4W_ROOT%\bin\py3_env.bat" echo py3_env.bat does not exist
+if exist "%OSGEO4W_ROOT%\bin\o4w_env.bat" call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 
-if exist %OSGEO4W_ROOT%\bin\o4w_env.bat call %OSGEO4W_ROOT%\bin\o4w_env.bat
-
-if not exist %OSGEO4W_ROOT%\bin\qt5_env.bat goto setqtenv
-call %OSGEO4W_ROOT%\bin\qt5_env.bat
+if not exist "%OSGEO4W_ROOT%\bin\qt5_env.bat" goto setqtenv
+call "%OSGEO4W_ROOT%\bin\qt5_env.bat"
 goto setpyenv
 
 :setqtenv
-if exist %QGISAPP%\qtplugins set QT_PLUGIN_PATH=%QGISAPP%\qtplugins;%QTAPP%\plugins
-if not exist %QGISAPP%\qtplugins echo QT env not set
+if exist "%QGISAPP%\qtplugins" set QT_PLUGIN_PATH=%QGISAPP%\qtplugins;%QTAPP%\plugins
+if not exist "%QGISAPP%\qtplugins" echo QT env not set
 
 :setpyenv
-if not exist %OSGEO4W_ROOT%\bin\py3_env.bat goto setpyenvexp
-call %OSGEO4W_ROOT%\bin\py3_env.bat
+if not exist "%OSGEO4W_ROOT%\bin\py3_env.bat" goto setpyenvexp
+call "%OSGEO4W_ROOT%\bin\py3_env.bat"
 goto startapp
 
 :setpyenvexp
-if exist %OSGEO4W_ROOT%\apps\%QGISAP%\bin PATH=%OSGEO4W_ROOT%\apps\%QGISAP%\bin;%OSGEO4W_ROOT%\bin;%PATH%
-if exist %OSGEO4W_ROOT%\apps\%QGISAP%\bin path %OSGEO4W_ROOT%\apps\%QGISAP%\bin;%PATH%
-if exist %QGISAPP%\bin set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/%QGISAP%
+if exist "%OSGEO4W_ROOT%\apps\%QGISAP%\bin" set PATH=%OSGEO4W_ROOT%\apps\%QGISAP%\bin;%OSGEO4W_ROOT%\bin;%PATH%
+if exist "%OSGEO4W_ROOT%\apps\%QGISAP%\bin" path %OSGEO4W_ROOT%\apps\%QGISAP%\bin;%PATH%
+if exist "%QGISAPP%\bin" set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/%QGISAP%
 set GDAL_FILENAME_IS_UTF8=YES
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
-if exist %OSGEO4W_ROOT%\apps\%QGISAP%\python set PYTHONPATH=%OSGEO4W_ROOT%\apps\%QGISAP%\python;%PYTHONPATH%
+if exist "%OSGEO4W_ROOT%\apps\%QGISAP%\python" set PYTHONPATH=%OSGEO4W_ROOT%\apps\%QGISAP%\python;%PYTHONPATH%
 
 :startapp
 echo QGIS installation to be used: %OSGEO4W_ROOT%
@@ -88,6 +102,18 @@ echo upgrading pip...
 python -m pip install --upgrade pip
 if %errorlevel% equ 0 (
  echo pip upgraded successfully
+)
+echo.
+echo upgrading wheel...
+python -m pip install --upgrade wheel
+if %errorlevel% equ 0 (
+ echo wheel upgraded successfully
+)
+echo.
+echo upgrading setuptools...
+python -m pip install --upgrade setuptools
+if %errorlevel% equ 0 (
+ echo setuptools upgraded successfully
 )
 echo.
 echo upgrading untangle...
@@ -112,12 +138,6 @@ echo upgrading pandas...
 python -m pip install --upgrade pandas
 if %errorlevel% equ 0 (
  echo pandas upgraded successfully
-)
-echo.
-echo upgrading setuptools...
-python -m pip install --upgrade setuptools
-if %errorlevel% equ 0 (
- echo setuptools upgraded successfully
 )
 echo.
 echo upgrading scikit-learn...
--- start_cft.bat
+++ start_cft.bat
@@ -3,56 +3,56 @@
 echo.
 echo CFT Startup Script
 echo.
+echo ______________________________
 chcp 437
 SET mypath=%~dp0
 set mypath=%mypath:~0,-1%
 
+rem load the environment
+if exist "%mypath%\qgis_env.bat" goto envexists
+echo qgis_env.bat not found in the CFT folder. run the install-qgis-modules.bat again
+pause
+exit
+
+:envexists
 call "%mypath%\qgis_env.bat"
-IF %errorlevel% NEQ 0 (
-  echo could not locate qgis_env.bat file, please run the installation script again
-  pause
-  exit
-)
-if %OSGEO4W_ROOT% == "" (
-  echo.
-  echo could not load the QGIS v3 environment.
-  pause
-  exit
-)
+if exist "%OSGEO4W_ROOT%"  goto qgisexists
+echo QGIS folder does not exist, check QGIS and run the install-qgis-modules.bat again
+pause
+exit
 
-if not exist %OSGEO4W_ROOT% echo %OSGEO4W_ROOT% does not exist
-if not exist %QGISAPP% echo %QGISAPP% does not exist
-if not exist %PYAPP% echo %PYAPP% does not exist
-if not exist %QTAPP% echo %QTAPP% does not exist
-if not exist %OSGEO4W_ROOT%\bin\o4w_env.bat echo o4w_env.bat does not exist
-if not exist %OSGEO4W_ROOT%\bin\qt5_env.bat echo qt5_env.bat does not exist
-if not exist %OSGEO4W_ROOT%\bin\py3_env.bat echo py3_env.bat does not exist
-
-if exist %OSGEO4W_ROOT%\bin\o4w_env.bat call %OSGEO4W_ROOT%\bin\o4w_env.bat
+:qgisexists
+if not exist "%QGISAPP%" echo QGISAPP folder does not exist
+if not exist "%PYAPP%" echo PYAPP folder does not exist
+if not exist "%QTAPP%" echo QTAPP folder does not exist
+if not exist "%OSGEO4W_ROOT%\bin\o4w_env.bat" echo o4w_env.bat does not exist
+if not exist "%OSGEO4W_ROOT%\bin\qt5_env.bat" echo qt5_env.bat does not exist
+if not exist "%OSGEO4W_ROOT%\bin\py3_env.bat" echo py3_env.bat does not exist
+if exist "%OSGEO4W_ROOT%\bin\o4w_env.bat" call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 
 set QT_AUTO_SCREEN_SCALE_FACTOR=1
-if not exist %OSGEO4W_ROOT%\bin\qt5_env.bat goto setqtenv
-call %OSGEO4W_ROOT%\bin\qt5_env.bat
+if not exist "%OSGEO4W_ROOT%\bin\qt5_env.bat" goto setqtenv
+call "%OSGEO4W_ROOT%\bin\qt5_env.bat"
 goto setpyenv
 
 :setqtenv
-if exist %QGISAPP%\qtplugins set QT_PLUGIN_PATH=%QGISAPP%\qtplugins;%QTAPP%\plugins
-if not exist %QGISAPP%\qtplugins echo QT env not set
+if exist "%QGISAPP%\qtplugins" set QT_PLUGIN_PATH=%QGISAPP%\qtplugins;%QTAPP%\plugins
+if not exist "%QGISAPP%\qtplugins" echo QT env not set
 
 :setpyenv
-if not exist %OSGEO4W_ROOT%\bin\py3_env.bat goto setpyenvexp
-call %OSGEO4W_ROOT%\bin\py3_env.bat
+if not exist "%OSGEO4W_ROOT%\bin\py3_env.bat" goto setpyenvexp
+call "%OSGEO4W_ROOT%\bin\py3_env.bat"
 goto startapp
 
 :setpyenvexp
-if exist %OSGEO4W_ROOT%\apps\%QGISAP%\bin PATH=%OSGEO4W_ROOT%\apps\%QGISAP%\bin;%OSGEO4W_ROOT%\bin;%PATH%
-if exist %OSGEO4W_ROOT%\apps\%QGISAP%\bin path %OSGEO4W_ROOT%\apps\%QGISAP%\bin;%PATH%
-if exist %QGISAPP%\bin set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/%QGISAP%
+if exist "%OSGEO4W_ROOT%\apps\%QGISAP%\bin" set PATH=%OSGEO4W_ROOT%\apps\%QGISAP%\bin;%OSGEO4W_ROOT%\bin;%PATH%
+if exist "%OSGEO4W_ROOT%\apps\%QGISAP%\bin" path %OSGEO4W_ROOT%\apps\%QGISAP%\bin;%PATH%
+if exist "%QGISAPP%\bin" set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/%QGISAP%
 set GDAL_FILENAME_IS_UTF8=YES
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
-if exist %OSGEO4W_ROOT%\apps\%QGISAP%\python set PYTHONPATH=%OSGEO4W_ROOT%\apps\%QGISAP%\python;%PYTHONPATH%
+if exist "%OSGEO4W_ROOT%\apps\%QGISAP%\python" set PYTHONPATH=%OSGEO4W_ROOT%\apps\%QGISAP%\python;%PYTHONPATH%
 
 :startapp
-echo.
-START /B python cft.py
+START /B pythonw cft.py
+exit
